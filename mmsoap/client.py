3a7ec08c75e54e2b0be366a8f88895ead10b3776
Jordan Trudgett <jordan.trudgett@messagemedia.com.au>
2014-04-09 12:03:19 +1000

    Add check_user, clean up
    Added license text, added check_user
    Added proxy support

--- mmsoap/client.py
+++ mmsoap/client.py
@@ -1,5 +1,45 @@
+"""
+    Copyright 2014 MessageMedia
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not
+    use this file except in compliance with the License.
+    You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
 import suds
 from cache import ExtendedObjectCache
+
+from suds.transport.http import HttpTransport as SudsHttpTransport
+
+class WellBehavedHttpTransport(SudsHttpTransport):
+    """HttpTransport which properly obeys the ``*_proxy`` environment variables."""
+
+    def u2handlers(self):
+        """Return a list of specific handlers to add.
+
+        The urllib2 logic regarding ``build_opener(*handlers)`` is:
+
+        - It has a list of default handlers to use
+
+        - If a subclass or an instance of one of those default handlers is given
+            in ``*handlers``, it overrides the default one.
+
+        Suds uses a custom {'protocol': 'proxy'} mapping in self.proxy, and adds
+        a ProxyHandler(self.proxy) to that list of handlers.
+        This overrides the default behaviour of urllib2, which would otherwise
+        use the system configuration (environment variables on Linux, System
+        Configuration on Mac OS, ...) to determine which proxies to use for
+        the current protocol, and when not to use a proxy (no_proxy).
+
+        Thus, passing an empty list will use the default ProxyHandler which
+        behaves correctly.
+        """
+        return []
 
 class MMSoapClient:
     WSDL_URL = "https://soap.m4u.com.au/?wsdl"
@@ -10,7 +50,7 @@ class MMSoapClient:
         if "cache_location" in kwargs:
             object_cache.setlocation(kwargs["cache_location"])
 
-        self.client = suds.client.Client(self.WSDL_URL, cache=object_cache)
+        self.client = suds.client.Client(self.WSDL_URL, cache=object_cache, transport=WellBehavedHttpTransport())
 
         self.authentication = None
         self.authentication = self.create("AuthenticationType")
@@ -28,6 +68,9 @@ class MMSoapClient:
 
     def set_password(self, password):
         self.authentication.password = password
+
+    def check_user(self):
+        return self.client.service.checkUser(self.authentication).accountDetails
 
     def send_messages(self, recipients, content):
         recipients_type = self.create("RecipientsType")
--- sample.py
+++ sample.py
@@ -1,16 +1,33 @@
-import mmsoap
+"""
+    Copyright 2014 MessageMedia
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not
+    use this file except in compliance with the License.
+    You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+import mmsoap
 
 # Create client, specifying cache_location (e.g. /tmp/mmsoap for Linux or C:/temp/mmsoap for Windows)
 client = mmsoap.MMSoapClient("your-userId", "your-password", cache_location="C:/temp/mmsoap")
 
+# Check the user
+print "Check User"
+details = client.check_user()
+print "Credit limit: %d / Credit remaining: %d" % (details._creditLimit, details._creditRemaining)
+
 # Send messages
-print "Send messages"
-result = client.send_messages(["+61412345689"], "Hello from MMSoap.py!")
-print "Send %d messages, scheduled %d messages, %d messages failed" % (result._sent, result._scheduled, result._failed)
+print "Send Messages"
+result = client.send_messages(["+61412345678"], "Hello from messagemedia-python!")
+print "Sent %d messages, scheduled %d messages, %d messages failed" % (result._sent, result._scheduled, result._failed)
 
 # Check for replies
-print "Replies:"
+print "Replies"
 replies = client.check_replies()
 
 for reply in replies:
