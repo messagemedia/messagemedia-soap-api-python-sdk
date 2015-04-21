1c920b0c7dbf1250062b2d4c8093c6b800f37939
Tristan Penman <tristan.penman@messagemedia.com.au>
2015-04-22 09:42:45 +1000

    Update project URL in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 setup(
     name='MMSoap',
     packages=find_packages(),
-    url='https://github.com/infoxchange/messagemedia-python',
+    url='https://github.com/messagemedia/messagemedia-python',
     license='Apache',
     description='This library provides a simple interface for sending and receiving messages using the MessageMedia SOAP API.',
     long_description=open('README.md').read(),
